4ee36c3c7e28dc35f7e1b871e20c2f244acf2b67
Jamie Whitney <thejamiewhitney@gmail.com>
2022-02-01 21:47:46 +0000

    Added docker-compose.yml

--- main.go
+++ main.go
@@ -3,17 +3,24 @@ package main
 import (
 	"context"
 	"fmt"
-	kafka "github.com/segmentio/kafka-go"
-	"time"
 	"strconv"
+	"time"
+
+	kafka "github.com/segmentio/kafka-go"
+)
+
+var (
+	brokers = []string{"192.168.1.171:9092", "192.168.1.172:9092", "192.168.1.173:9092"}
+	topic = "test-topic"
+	GroupID = "test-group"
 )
 
 func main() {
 	go Producer()
 	r := kafka.NewReader(kafka.ReaderConfig{
-		Brokers: []string{"192.168.1.171:9092", "192.168.1.172:9092", "192.168.1.173:9092"},
-		Topic:   "test-topic",
-		GroupID: "test-group",
+		Brokers: brokers,
+		Topic:  topic,
+		GroupID: GroupID,
 	})
 
 	for {
@@ -31,24 +38,24 @@ func main() {
 
 func Producer() {
 	i := 0
-	
+
 	w := kafka.NewWriter(kafka.WriterConfig{
-		Brokers: []string{"192.168.1.172:9092", "192.168.1.173:9092", "192.168.1.171:9092"},
-		Topic:   "test-topic",
+		Brokers: brokers,
+		Topic:   topic,
 	})
 	ctx := context.Background()
-	
+
 	for {
 		err := w.WriteMessages(ctx, kafka.Message{
-			Key: []byte(strconv.Itoa(i)),
+			Key:   []byte(strconv.Itoa(i)),
 			Value: []byte("this is message" + strconv.Itoa(i)),
 		})
 		if err != nil {
 			panic("could not write message " + err.Error())
 		}
-	
+
 		fmt.Println("writes:", i)
 		i++
 		time.Sleep(time.Second)
 	}
-}	
+}
